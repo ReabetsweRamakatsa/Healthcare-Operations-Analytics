3aa9ffc4766e767a62d37a02ac4e5b1c332363dd
Reabetswe Ramakatsa <reabetsweramakatsa@gmail.com>
2026-01-12 19:56:38 +0200

    Update analysis_queries.sql

--- sql/analysis_queries.sql
+++ sql/analysis_queries.sql
@@ -1,14 +1,14 @@
 
 -- Average length of stay by department
 SELECT d.department_name,
-       AVG(dis.discharge_date - a.admission_date) AS avg_length_of_stay
+       AVG(DATEDIFF(dis.discharge_date, a.admission_date)) AS avg_length_of_stay
 FROM admissions a
 JOIN discharges dis ON a.admission_id = dis.admission_id
 JOIN departments d ON a.department_id = d.department_id
 GROUP BY d.department_name;
 
 -- Admission trends over time
-SELECT DATE_TRUNC('month', admission_date) AS month,
+SELECT DATE_FORMAT(admission_date, '%Y-%m-01') AS month,
        COUNT(*) AS admissions
 FROM admissions
 GROUP BY month
